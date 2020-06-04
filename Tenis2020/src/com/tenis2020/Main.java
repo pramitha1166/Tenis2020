eb1c61a6325b41be6d176d1fda7d76076cee1f0b
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2020-06-04 11:20:33 +0530

    add JFrame

---
+++ Tenis2020/src/com/tenis2020/Main.java
@@ -0,0 +1,17 @@
+package com.tenis2020;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame frame = new JFrame("Mini Tennis"); 
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
